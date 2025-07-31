2607b6c9a64b0b00d7d59931ec49e255fff37b34
나제법 <skwpqjq@gmail.com>
2025-07-31 15:31:47 +0900

    Initial commit

---
+++ src/main/java/com/ssafy/c204_be_judge/C204BeJudgeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.c204_be_judge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C204BeJudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C204BeJudgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/c204_be_judge/C204BeJudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.c204_be_judge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C204BeJudgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
