4746dc94b1062f420946ab96ec0a7c6e87ac3c38
나제법 <skwpqjq@gmail.com>
2025-08-04 16:45:59 +0900

    feat: 채점 요청 DTO 생성

---
+++ src/main/java/com/ssafy/c204_be_judge/rabbitmq/message/JudgeMessage.java
@@ -0,0 +1,16 @@
+package com.ssafy.c204_be_judge.rabbitmq.message;
+
+import lombok.*;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JudgeMessage {
+
+    private Long problemId;
+    private String programmingLanguage;
+    private String sourceCode;
+
+}
