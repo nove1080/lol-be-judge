a0401ac592e5f164c6118932ac7d027930013fe6
나제법 <skwpqjq@gmail.com>
2025-08-10 18:26:38 +0900

    refactor: 환경변수 적용 및 로깅 수준 info로 변경

--- src/main/java/com/ssafy/c204_be_judge/judge/service/JudgeService.java
+++ src/main/java/com/ssafy/c204_be_judge/judge/service/JudgeService.java
@@ -8,11 +8,14 @@ import com.ssafy.c204_be_judge.judge.worker.JudgeWorker;
 import com.ssafy.c204_be_judge.validation.exception.JudgeServerException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
@@ -21,6 +24,11 @@ public class JudgeService {
     private final SQSService sqsService;
     private final JudgeWorker judgeWorker;
     private final RestClient restClient;
+
+    @Value("${judge.api-server.base-url}")
+    private String apiServerBaseUrl;
+    @Value("${judge.api-server.result-path}")
+    private String judgeResultApiPath;
 
     public JudgeService(@Qualifier("judgeWorkerV3") JudgeWorker judgeWorker, SQSService sqsService, RestClient restClient) {
         this.judgeWorker = judgeWorker;
@@ -35,18 +43,18 @@ public class JudgeService {
         JudgeResult result = judgeWorker.run(JudgeCommand.from(message));
         Long end = System.currentTimeMillis();
 
-        log.debug("채점 소요 시간: {}ms", end - start);
+        log.info("채점 소요 시간: {}ms | memberId: {} | problemId: {} | 시간: {}", end - start, message.memberId(), message.problemId(), LocalDateTime.now());
         sendToAPIServer(message.memberId(), result);
         return result;
     }
 
     private void sendToAPIServer(Long memberId, JudgeResult result) {
         restClient.post()
-                .uri("http://localhost:8081/api/v1/problems/judge-result/" + memberId)
+                .uri(apiServerBaseUrl + judgeResultApiPath + memberId)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(result)
                 .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+                .onStatus(HttpStatusCode::isError, (request, response) -> {
                     throw new JudgeServerException("채점 결과 전송에 실패했습니다. 코드: " + response.getStatusCode());
                 })
                 .toBodilessEntity();
