5e3c75821dc08276950b7923d406d2f74965010e
나제법 <skwpqjq@gmail.com>
2025-08-11 14:18:22 +0900

    refactor: 자바 채점 워커 구현

--- src/main/java/com/ssafy/c204_be_judge/judge/worker/JudgeWorkerForJava.java
+++ src/main/java/com/ssafy/c204_be_judge/judge/worker/JudgeWorkerForJava.java
@@ -8,9 +8,11 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -23,21 +25,11 @@ import java.util.stream.IntStream;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class JudgeWorkerForUbuntu implements JudgeWorker {
-
-    private static final int MAX_THREAD_POOL_SIZE = Math.max(1, Runtime.getRuntime().availableProcessors());
-
-    private static final String HOME_DIR = System.getProperty("user.home"); // /home/ubuntu
-    private static final String ISOLATE_ROOT = "/var/lib/isolate/";
-    private static final String TESTCASE_PATH = "testcases/";
-
-    private static final String OUTPUT_FILE_SUFFIX = "_output.txt";
-    private static final String ERROR_FILE_SUFFIX = "_err.txt";
-    private static final String META_FILE_SUFFIX = "_meta.txt";
+public class JudgeWorkerForJava extends JudgeWorker {
 
     @PostConstruct
     public void init() {
-        log.info("JudgeWorkerV3 initialized with THREAD_POOL_SIZE={}", MAX_THREAD_POOL_SIZE);
+        log.debug("JudgeWorkerForJava initialized with THREAD_POOL_SIZE={}", MAX_THREAD_POOL_SIZE);
     }
 
     public JudgeResult run(JudgeCommand judgeCommand) {
@@ -108,24 +100,6 @@ public class JudgeWorkerForUbuntu implements JudgeWorker {
         return checkAnswer(judgeCommand, boxId, testcase);
     }
 
-    // 초기화 작업
-    private static void prepareSandbox(int boxId) {
-        ProcessBuilder pb = new ProcessBuilder(
-                "sudo",
-                "isolate",
-                "--box-id=" + boxId,
-                "--init"
-        );
-
-        try {
-            Process process = pb.start();
-            logging(process);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-    }
-
     private static String writeSourceCode(JudgeCommand judgeCommand) {
         final String codePath = HOME_DIR + "/sourceCode/" + judgeCommand.problemId() + "/Main.java";
 
@@ -165,17 +139,6 @@ public class JudgeWorkerForUbuntu implements JudgeWorker {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
-    }
-
-    private static int getTotalTestcaseCount(JudgeCommand judgeCommand) {
-        final String testcasePath = HOME_DIR + "/testcases/" + judgeCommand.problemId();
-
-        File dir = new File(testcasePath);
-        if (!dir.exists() || !dir.isDirectory()) {
-            throw new IllegalArgumentException("디렉토리가 유효하지 않습니다! [경로: %s]".formatted(testcasePath));
-        }
-
-        return dir.list((f, name) -> name.contains(".in")).length;
     }
 
     /**
@@ -240,99 +203,4 @@ public class JudgeWorkerForUbuntu implements JudgeWorker {
         return "";
     }
 
-    private static boolean checkAnswer(JudgeCommand command, int boxId, int testcaseNum) {
-        final String answerFile = HOME_DIR + "/" + TESTCASE_PATH + command.problemId() + "/" + testcaseNum + ".out";
-        final String outputFile = getSandboxPath(boxId) + testcaseNum + OUTPUT_FILE_SUFFIX;
-
-        ProcessBuilder pb = new ProcessBuilder(
-                "sudo",
-                "diff", "-q", outputFile, answerFile
-        );
-
-        boolean isSolved = true;
-        try {
-            Process process = pb.start();
-            logging(process);
-
-            int exitCode = process.waitFor();
-            if (exitCode == 1) isSolved = false;
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-
-        return isSolved;
-    }
-
-    /**
-     * 메타 파일을 읽어 테스트케이스 결과를 생성합니다.
-     * @param testcaseNum 테스트케이스 번호
-     * @return TestcaseResult
-     */
-    private static TestcaseResult readMetaFile(int testcaseNum) {
-        final String metaFilePath = "meta/" + testcaseNum + META_FILE_SUFFIX;
-
-        double runningTime = 0.0;
-        int memoryUsage = 0;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(metaFilePath))) {
-            String line;
-            String errorCode = "";
-            String errorMessage = "";
-            while ((line = reader.readLine()) != null) {
-                if (line.startsWith("status:")) {
-                    errorCode = line.split(":", 2)[1].trim();
-                } else if (line.startsWith("message:")) {
-                    errorMessage = line.split(":", 2)[1].trim();
-                } else if (line.startsWith("time-wall:")) {
-                    runningTime = Double.parseDouble(line.split(":", 2)[1].trim());
-                } else if (line.startsWith("max-rss:")) {
-                    memoryUsage = Integer.parseInt(line.split(":", 2)[1].trim());
-                }
-            }
-
-            if (StringUtils.hasText(errorCode)) {
-                return TestcaseResult.fail(testcaseNum, errorMessage);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-
-        return TestcaseResult.success(testcaseNum, runningTime, memoryUsage);
-    }
-
-    private static void cleanup(int boxId) {
-        ProcessBuilder pb = new ProcessBuilder(
-                "sudo",
-                "isolate",
-                "--box-id=" + boxId,
-                "--cleanup"
-        );
-
-        try {
-            Process process = pb.start();
-            logging(process);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static String getSandboxPath(int boxId) {
-        return ISOLATE_ROOT + boxId + "/box/";
-    }
-
-    private static void logging(Process process) throws IOException {
-        BufferedReader stdOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        BufferedReader stdErr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-
-        String line;
-        while ((line = stdOut.readLine()) != null) {
-            log.debug(line);
-        }
-        while ((line = stdErr.readLine()) != null) {
-            log.debug(line);
-        }
-    }
 }
