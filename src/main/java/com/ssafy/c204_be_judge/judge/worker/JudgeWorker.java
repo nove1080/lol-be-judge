e3d146b36ad39595e86ee89164d700acf771477a
나제법 <89574219+nove1080@users.noreply.github.com>
2026-04-24 20:50:45 +0900

    refactor: 채점 워커 보일러 플레이트 코드 제거 (#1)
    * refactor(JudgeWorker): 보일러 플레이트 코드 제거
    
    - 템플릿 메서드 패턴 적용
    
    * chore(setting): 사용하지 않는 설정 제거
    
    - DB 설정 제거

--- src/main/java/com/ssafy/c204_be_judge/judge/worker/JudgeWorker.java
+++ src/main/java/com/ssafy/c204_be_judge/judge/worker/JudgeWorker.java
@@ -4,6 +4,11 @@ import com.ssafy.c204_be_judge.judge.command.JudgeCommand;
 import com.ssafy.c204_be_judge.judge.domain.JudgeResult;
 import com.ssafy.c204_be_judge.judge.domain.TestcaseResult;
 import com.ssafy.c204_be_judge.validation.exception.CompileException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.IntStream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 
@@ -22,11 +27,97 @@ public abstract class JudgeWorker {
     protected static final String ERROR_FILE_SUFFIX = "_err.txt";
     protected static final String META_FILE_SUFFIX = "_meta.txt";
 
-    public abstract JudgeResult run(JudgeCommand judgeCommand);
+    protected abstract String getFileName();
+    protected abstract List<String> getCompileCommand(String filePath);
+    protected abstract String getCompiledFilePath(String filePath);
+    protected abstract List<String> getRunCommand(JudgeCommand judgeCommand, int boxId, int testcaseNum);
 
-    protected abstract String compile(String filePath) throws CompileException;
+    public JudgeResult run(JudgeCommand judgeCommand) {
+        List<TestcaseResult> testcaseResults = new ArrayList<>();
 
-    protected abstract String executeSourceCode(JudgeCommand judgeCommand, int boxId, int testcaseNum);
+        try {
+            String filePath = writeSourceCode(judgeCommand, getFileName());
+            String classFilePath = compile(filePath);
+            int totalTestcaseCount = getTotalTestcaseCount(judgeCommand);
+
+            prepareJudge(classFilePath);
+
+            for (int start = 1; start <= totalTestcaseCount; start += MAX_THREAD_POOL_SIZE) {
+                int end = Math.min(start + MAX_THREAD_POOL_SIZE - 1, totalTestcaseCount);
+                testcaseResults.addAll(runTestcases(judgeCommand, start, end));
+            }
+        } catch (CompileException e) {
+            return JudgeResult.fail(judgeCommand, "컴파일에 실패하였습니다.");
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return JudgeResult.fail(judgeCommand, "채점 중 오류가 발생하였습니다.");
+        }
+
+        return JudgeResult.success(judgeCommand, testcaseResults);
+    }
+
+    protected String compile(String filePath) throws CompileException {
+        ProcessBuilder pb = new ProcessBuilder(getCompileCommand(filePath));
+
+        try {
+            Process process = pb.start();
+            logging(process);
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new CompileException("컴파일에 실패하였습니다. [경로: %s]".formatted(filePath));
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            throw new CompileException("컴파일에 실패하였습니다. [경로: %s]".formatted(filePath), e);
+        }
+
+        return getCompiledFilePath(filePath);
+    }
+
+    private static void prepareJudge(String codePath) throws InterruptedException, ExecutionException {
+        new ForkJoinPool(MAX_THREAD_POOL_SIZE).submit(() ->
+            IntStream.rangeClosed(1, MAX_THREAD_POOL_SIZE).parallel()
+                .forEach(threadNumber -> {
+                        prepareSandbox(threadNumber);
+                        sendSourceCode(threadNumber, codePath);
+                    }
+                )
+        ).get();
+    }
+
+    /**
+     * 주어진 범위의 테스트케이스를 병렬로 실행합니다. <br>
+     * 병렬처리 크기는 {@link #MAX_THREAD_POOL_SIZE}로 제한됩니다.
+     * @param judgeCommand 채점 명령
+     * @param start 시작 테스트케이스 번호
+     * @param end 종료 테스트케이스 번호
+     * @return 테스트케이스 결과 리스트
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    private List<TestcaseResult> runTestcases(JudgeCommand judgeCommand, int start, int end) throws InterruptedException, ExecutionException {
+        return new ForkJoinPool(MAX_THREAD_POOL_SIZE).submit(() ->
+            IntStream.rangeClosed(start, end)
+                .parallel()
+                .mapToObj(testcase -> {
+                    int boxId = testcase % MAX_THREAD_POOL_SIZE + 1;
+                    boolean isSolved = runTestcase(judgeCommand, boxId, testcase);
+
+                    if (isSolved) {
+                        return readMetaFile(testcase);
+                    } else {
+                        return TestcaseResult.fail(testcase);
+                    }
+                })
+                .toList()
+        ).get();
+    }
+
+    private boolean runTestcase(JudgeCommand judgeCommand, int boxId, int testcase) {
+        executeSourceCode(judgeCommand, boxId, testcase);
+        return checkAnswer(judgeCommand, boxId, testcase);
+    }
 
     protected static void prepareSandbox(int boxId) {
         ProcessBuilder pb = new ProcessBuilder(
@@ -158,6 +249,20 @@ public abstract class JudgeWorker {
         }
 
         return TestcaseResult.success(testcaseNum, runningTime, memoryUsage);
+    }
+
+    private String executeSourceCode(JudgeCommand judgeCommand, int boxId, int testcaseNum) {
+        ProcessBuilder pb = new ProcessBuilder(getRunCommand(judgeCommand, boxId, testcaseNum));
+
+        try {
+            Process process = pb.start();
+            logging(process);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        return "";
     }
 
     protected static void cleanup(int boxId) {
--- src/main/java/com/ssafy/c204_be_judge/judge/worker/JudgeWorkerForJava.java
+++ src/main/java/com/ssafy/c204_be_judge/judge/worker/JudgeWorkerForJava.java
@@ -1,20 +1,13 @@
 package com.ssafy.c204_be_judge.judge.worker;
 
 import com.ssafy.c204_be_judge.judge.command.JudgeCommand;
-import com.ssafy.c204_be_judge.judge.domain.JudgeResult;
-import com.ssafy.c204_be_judge.judge.domain.TestcaseResult;
 import com.ssafy.c204_be_judge.validation.exception.CompileException;
 import jakarta.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.stream.IntStream;
 
 /**
  * Ubuntu 환경에서 채점을 수행하는 JudgeWorker 구현체
@@ -31,134 +24,44 @@ public class JudgeWorkerForJava extends JudgeWorker {
         log.debug("JudgeWorkerForJava initialized with THREAD_POOL_SIZE={}", MAX_THREAD_POOL_SIZE);
     }
 
-    public JudgeResult run(JudgeCommand judgeCommand) {
-        List<TestcaseResult> testcaseResults = new ArrayList<>();
-
-        try {
-            String javaFilePath = writeSourceCode(judgeCommand, JAVA_FILE_NAME);
-            String classFilePath = compile(javaFilePath);
-            int totalTestcaseCount = getTotalTestcaseCount(judgeCommand);
-
-            prepareJudge(classFilePath);
-
-            for (int start = 1; start <= totalTestcaseCount; start += MAX_THREAD_POOL_SIZE) {
-                int end = Math.min(start + MAX_THREAD_POOL_SIZE - 1, totalTestcaseCount);
-                testcaseResults.addAll(runTestcases(judgeCommand, start, end));
-            }
-        } catch (CompileException e) {
-            return JudgeResult.fail(judgeCommand, "컴파일에 실패하였습니다.");
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            return JudgeResult.fail(judgeCommand, "채점 중 오류가 발생하였습니다.");
-        }
-
-        return JudgeResult.success(judgeCommand, testcaseResults);
+    @Override
+    protected String getFileName() {
+        return JAVA_FILE_NAME;
     }
 
-    /**
-     * 주어진 범위의 테스트케이스를 병렬로 실행합니다. <br>
-     * 병렬처리 크기는 {@link #MAX_THREAD_POOL_SIZE}로 제한됩니다.
-     * @param judgeCommand 채점 명령
-     * @param start 시작 테스트케이스 번호
-     * @param end 종료 테스트케이스 번호
-     * @return 테스트케이스 결과 리스트
-     * @throws InterruptedException
-     * @throws ExecutionException
-     */
-    private List<TestcaseResult> runTestcases(JudgeCommand judgeCommand, int start, int end) throws InterruptedException, ExecutionException {
-        return new ForkJoinPool(MAX_THREAD_POOL_SIZE).submit(() ->
-                IntStream.rangeClosed(start, end)
-                        .parallel()
-                        .mapToObj(testcase -> {
-                            int boxId = testcase % MAX_THREAD_POOL_SIZE + 1;
-                            boolean isSolved = runTestcase(judgeCommand, boxId, testcase);
-
-                            if (isSolved) {
-                                return readMetaFile(testcase);
-                            } else {
-                                return TestcaseResult.fail(testcase);
-                            }
-                        })
-                        .toList()
-        ).get();
+    @Override
+    protected List<String> getCompileCommand(String filePath) {
+        return List.of("javac", "-J-Xms1024m", "-J-Xmx1920m", "-J-Xss512m", "-encoding", "UTF-8", filePath);
     }
 
-    private static void prepareJudge(String codePath) throws InterruptedException, ExecutionException {
-        new ForkJoinPool(MAX_THREAD_POOL_SIZE).submit(() ->
-                IntStream.rangeClosed(1, MAX_THREAD_POOL_SIZE).parallel()
-                        .forEach(threadNumber -> {
-                                    prepareSandbox(threadNumber);
-                                    sendSourceCode(threadNumber, codePath);
-                                }
-                        )
-        ).get();
-    }
-
-    private boolean runTestcase(JudgeCommand judgeCommand, int boxId, int testcase) {
-        executeSourceCode(judgeCommand, boxId, testcase);
-        return checkAnswer(judgeCommand, boxId, testcase);
-    }
-
-    /**
-     * 주어진 Java 파일을 컴파일합니다.
-     * @param filePath 컴파일할 Java 파일의 경로
-     * @return 컴파일된 클래스 파일의 경로
-     * @throws CompileException 컴파일 실패 시 예외 발생
-     */
-    protected String compile(String filePath) throws CompileException {
-        ProcessBuilder pb = new ProcessBuilder(
-                "javac", "-J-Xms1024m", "-J-Xmx1920m", "-J-Xss512m", "-encoding", "UTF-8", filePath
-        );
-
-        try {
-            Process process = pb.start();
-            logging(process);
-
-            int exitCode = process.waitFor();
-            if (exitCode != 0) {
-                throw new CompileException("컴파일에 실패하였습니다. [경로: %s]".formatted(filePath));
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            throw new CompileException("컴파일에 실패하였습니다. [경로: %s]".formatted(filePath), e);
-        }
-
+    @Override
+    protected String getCompiledFilePath(String filePath) {
         return filePath.replaceAll("\\.java$", ".class");
     }
 
-    protected String executeSourceCode(JudgeCommand judgeCommand, int boxId, int testcaseNum) {
+    @Override
+    protected List<String> getRunCommand(JudgeCommand judgeCommand, int boxId, int testcaseNum) {
         final String testcasePath = HOME_DIR + "/" + TESTCASE_PATH + judgeCommand.problemId();
 
-        ProcessBuilder pb = new ProcessBuilder(
-                "sudo",
-                "isolate",
-                "--box-id=" + boxId,
-                "--processes=128",
-                "--dir=/usr/lib/jvm",
-                "--dir=/etc/java-17-openjdk/security",
-                "--dir=" + testcasePath,
-                "--stdin=" + testcasePath + "/" + testcaseNum + ".in",
-                "--stdout=" + testcaseNum + OUTPUT_FILE_SUFFIX,
-                "--stderr=" + testcaseNum + ERROR_FILE_SUFFIX,
-                "--time=" + judgeCommand.timeLimit() * 2 + 1, // 2배 + 1초 여유
-                "--mem=" + 16384000, //16GB
-                "--meta=" + "meta/" + testcaseNum + META_FILE_SUFFIX,
-                "--run",
-                "--",
-                "/usr/lib/jvm/java-17-openjdk-arm64/bin/java",
-                "-Xms1024m", "-Xmx1920m", "-Xss512m", "-Dfile.encoding=UTF-8", "-XX:+UseSerialGC",
-                "Main"
+        return List.of(
+            "sudo",
+            "isolate",
+            "--box-id=" + boxId,
+            "--processes=128",
+            "--dir=/usr/lib/jvm",
+            "--dir=/etc/java-17-openjdk/security",
+            "--dir=" + testcasePath,
+            "--stdin=" + testcasePath + "/" + testcaseNum + ".in",
+            "--stdout=" + testcaseNum + OUTPUT_FILE_SUFFIX,
+            "--stderr=" + testcaseNum + ERROR_FILE_SUFFIX,
+            "--time=" + judgeCommand.timeLimit() * 2 + 1, // 2배 + 1초 여유
+            "--mem=" + 16384000, //16GB
+            "--meta=" + "meta/" + testcaseNum + META_FILE_SUFFIX,
+            "--run",
+            "--",
+            "/usr/lib/jvm/java-17-openjdk-arm64/bin/java",
+            "-Xms1024m", "-Xmx1920m", "-Xss512m", "-Dfile.encoding=UTF-8", "-XX:+UseSerialGC",
+            "Main"
         );
-
-        try {
-            Process process = pb.start();
-            logging(process);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-
-        return "";
     }
-
 }
